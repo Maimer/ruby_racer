2bdc4135666fc39f16b1cc3cc552cf62e386ed81
Maimer <nesnl23@yahoo.com>
2014-06-07 20:08:24 -0400

    Added falling images for left and right

--- player.rb
+++ player.rb
@@ -1,8 +1,10 @@
 class Player
   def initialize(window, tile)
     @window = window
-    @icon = Gosu::Image.new(@window, "playerright.png", true)
-    @iconleft = Gosu::Image.new(@window, "playerleft.png", true)
+    @icon = Gosu::Image.new(@window, "runright/playerright.png", true)
+    @iconleft = Gosu::Image.new(@window, "runleft/playerleft.png", true)
+    @fallright = Gosu::Image.new(@window, "runright/fallright.png", true)
+    @fallleft = Gosu::Image.new(@window, "runleft/fallleft.png", true)
     @runright = []
     12.times do |i|
       @runright << Gosu::Image.new(@window, "runright/right#{i+1}.png", true)
@@ -15,6 +17,7 @@ class Player
     @y = (tile.height * 8) - @icon.height
     @tile = tile
     @direction = 1
+    @falling = false
   end
 
   def move_left(board, offset)
@@ -43,11 +46,13 @@ class Player
 
   def floor_contact(board, offset, speed, height)
     @y += 2 * speed
+    @falling = true
     board.each do |tile|
       if (tile.y + offset) > @y && (tile.y + offset) - @y < @icon.height
         if tile.x > 0 && tile.x < @window.width - @tile.width
           if tile.x > @x - @tile.width && tile.x < @x + @icon.width
             @y = (tile.y + offset) - @icon.height - 1
+            @falling = false
           end
         end
       end
@@ -55,19 +60,24 @@ class Player
 
     if @y > height - @icon.height
       @y = height - @icon.height
+      @falling = false
     end
   end
 
   def draw(movement)
     if @direction == 1
-      if movement == 0
+      if @falling == true
+        @fallright.draw(@x, @y, 1)
+      elsif movement == 0
         @icon.draw(@x, @y, 1)
       else
         num = (movement / 3) - 1
         @runright[num].draw(@x, @y, 1)
       end
     elsif @direction == -1
-      if movement == 0
+      if @falling == true
+        @fallleft.draw(@x, @y, 1)
+      elsif movement == 0
         @iconleft.draw(@x, @y, 1)
       else
         num = (movement / 3) - 1
